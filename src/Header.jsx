1f72b50c9b30d768c94e174f7f5bc14c5d3f2b82
ouazou99 <59511349+ouazou99@users.noreply.github.com>
2024-12-21 19:20:09 -0500

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1> Keeper app </h1>
+      </header>
+    </div>
+  );
+}
+export default Header;
---
+++ src/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> this is the note title </h1>
+      <p> this is the note Content</p>
+    </div>
+  );
+}
+export default Note;
